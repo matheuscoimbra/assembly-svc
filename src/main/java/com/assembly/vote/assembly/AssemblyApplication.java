14ed8e95fda3c829a2aff9368ac676c194db3953
matheus <matheus.moraes@hospibalsaodomingos.com.br>
2021-03-06 11:46:44 -0300

    Initial commit

---
+++ src/main/java/com/assembly/vote/assembly/AssemblyApplication.java
@@ -0,0 +1,13 @@
+package com.assembly.vote.assembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssemblyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assembly/vote/assembly/AssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assembly.vote.assembly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
